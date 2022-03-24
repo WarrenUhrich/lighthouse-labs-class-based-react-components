7f6bbe81867f4e1251c26cd77f65a0f8a5079b32
Warren <hello@warren.codes>
2022-03-24 11:55:14 -0600

    Add increaseCount method to ClassCounter component (shows this binding concern.)

--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -8,13 +8,18 @@ class ClassCounter extends Component {
         this.state = {
             count: 0
         };
+
+        this.increaseCount = this.increaseCount.bind(this); // Binds "this", so it is not out-of-context in DOM mention.
     }
 
+    increaseCount(){
+        this.setState({count: this.state.count + 1});
+    }
 
     render(){ // Render must return your component output!
         return (<>
             <h1>{this.props.heading}</h1>
-            <button onClick={e => {this.setState({count: this.state.count + 1})}}>
+            <button onClick={this.increaseCount}>
                 Current count is {this.state.count}
             </button>
         </>);
